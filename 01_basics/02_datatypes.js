e0f96d40b6d931d66026f8e04d6c124e4fa397c3
prinsi124 <prinsigediya10@gmail.com>
2025-05-21 10:07:56 +0000

    datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict";  // treat all JS code has never version
+
+// alert (3 = 3) // we are using nodejs, not browser
+
+console.log(3 + 3)
+
+console.log("prinsi")
+
+let name = "prinsi"
+let age = 19
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+//object
+
+console.log(typeof undefind); //undefind
+console.log(typeof null); //object
