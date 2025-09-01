6bfe38ba17187668c4c57450e342a1dccdbdd2b9
prinsi124 <prinsigediya10@gmail.com>
2025-09-01 10:37:55 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "prinsi"
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber );
+//console.log(valueInNumber );
+
+// "33" => 33
+// "33abcd" => NaN
+// true => 1 ; false => 0
+
+let isLoggedIn = "prinsi"
+
+let booleanIsLoggedIn = Boolean (isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true ,0 =>false
+// "" => false
+// "prinsi" => true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
