5f04de9c72d67e9c6b205ba1b72c4a05448f5533
prinsi124 <prinsigediya10@gmail.com>
2025-09-09 05:18:58 +0000

    dates

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,30 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toTimeString());
+
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2025, 0, 25)
+// console.log(myCreatedDate.toDateString());
+
+// let myCreatedDate = new Date(2025, 0, 25, 5, 3)
+// console.log(myCreatedDate.toLocaleString());
+
+// let myCreatedDate = new Date("2025-01-03")
+// console.log(myCreatedDate.toLocaleString());
+
+let myCreatedDate = new Date("01-14-2025")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
