3d909be3d4a51b8c999625e0acc90c8ad322ff8b
prinsi124 <prinsigediya10@gmail.com>
2025-09-10 04:40:22 +0000

    dates done

--- 01_basics/07_datesinJs.js
+++ 01_basics/07_datesinJs.js
@@ -28,3 +28,14 @@ let myTimeStamp = Date.now()
 // console.log(myCreatedDate.getTime());
 // console.log(Math.floor(Date.now()/1000));
 
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
+// `${newDate.getDay()} and the time`
+
+newDate.toLocaleString('default', {
+    weekday: "long",
+    
+})
