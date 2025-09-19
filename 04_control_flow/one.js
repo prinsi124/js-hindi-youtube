ac73b1a69459eb1898d82cf182776a9a814fade6
prinsi124 <prinsigediya10@gmail.com>
2025-09-19 06:42:09 +0000

    one

---
+++ 04_control_flow/one.js
@@ -0,0 +1,34 @@
+// pura code execute nahi hona chaiye conditional base pr hoga chaiye logic pr 
+
+// if
+
+// if (true) {                // true he to code execute hoga false me nahi hoga 
+
+// }
+
+const isUserLoggedIn = true          // ae = assign kiya he oper
+const temperature = 41 
+
+// if( temperature === 40 ) {              // condition comparison 
+//     console.log("less than 50");
+// } else {
+//     console.log("temperature is greater than 50")
+// }
+
+// console.log("execute")
+
+// comparison ke opeeator = <, >, <=, >=, ==: check krne ke liye, !=, ===: type check kre, != 
+
+
+// scope releated hee imp 
+
+// const score = 200
+
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`user power: ${power}`);
+// }
+
+// console.log(`user power: ${power}`);
+
+
